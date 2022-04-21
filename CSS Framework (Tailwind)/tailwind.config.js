b005e37e14d41dfe2a7486254f483b803536f61e
Daffa <daffaerpan@gmail.com>
2022-04-21 08:59:26 +0700

    learning Tailwind

---
+++ CSS Framework (Tailwind)/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily : {
+        Lora : [ 'Lora'],
+        Peg : ['Square Peg']
+      },
+      spacing : {
+        13 : '13px'
+      }
+    },
+  },
+  plugins: [],
+}
